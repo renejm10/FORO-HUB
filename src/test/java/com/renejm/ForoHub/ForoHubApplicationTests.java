cda76c2a143b6f7bec73cad9dbb464e08ce660a4
Daniel Jimenez <rene.jm10@gmail.com>
2024-06-27 17:08:23 -0600

    creacion de repositorio con archivos base

---
+++ src/test/java/com/renejm/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renejm.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
