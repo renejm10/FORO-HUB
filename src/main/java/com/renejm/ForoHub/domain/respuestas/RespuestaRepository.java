7a191740226665a3063dcbb7f3a40c981569a4b8
Daniel Jimenez <rene.jm10@gmail.com>
2024-06-27 22:00:17 -0600

    Creacion de repositorios

---
+++ src/main/java/com/renejm/ForoHub/domain/respuestas/RespuestaRepository.java
@@ -0,0 +1,7 @@
+package com.renejm.ForoHub.domain.respuestas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+
+}
